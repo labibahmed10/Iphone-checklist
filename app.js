c46aa0bcb1f28ed82d915a2a53c8788b5ea026aa
labibahmed10 <labib.ahmed.372@gmail.com>
2022-02-13 16:48:31 +0600

    updating

--- app.js
+++ app.js
@@ -1,50 +1,69 @@
-//phoneprice
-const iphone11PriceText = document.getElementById("11-price");
-const iphone11Price = parseFloat(iphone11PriceText.innerText);
+//phone
+const phonePriceText = document.getElementById("11-price");
+const phonePrice = parseFloat(phonePriceText.innerText);
 
 const phoneQuantityText = document.getElementById("phone-quantity");
 let phoneQuantity = parseInt(phoneQuantityText.value);
 
-function updatePhonePrice() {
-  if (increase == true) {
-    iphone11PriceText.innerText = iphone11Price * ++phoneQuantity;
-    phoneQuantityText.value = phoneQuantity;
-  } else {
-    iphone11PriceText.innerText = iphone11Price * --phoneQuantity;
-    phoneQuantityText.value = phoneQuantity;
-  }
-}
-
 document.getElementById("phone-plus").addEventListener("click", function () {
-  updatePhonePrice(true);
+  updatePhonePrice(true, phonePriceText, phonePrice, phoneQuantityText, phoneQuantity++);
 });
 
 document.getElementById("phone-minus").addEventListener("click", function () {
-  updatePhonePrice(false);
+  updatePhonePrice(false, phonePriceText, phonePrice, phoneQuantityText, phoneQuantity--);
 });
 
-//caseprice
-
+//case
 const casePriceText = document.getElementById("case-price");
 const casePrice = parseFloat(casePriceText.innerText);
 
 const caseQuantityText = document.getElementById("case-quantity");
 let caseQuantity = parseInt(caseQuantityText.value);
 
-function updateCaseNumber(increase) {
-  if (increase == true) {
-    casePriceText.innerText = casePrice * ++caseQuantity;
-    caseQuantityText.value = caseQuantity;
-  } else {
-    casePriceText.innerText = casePrice * --caseQuantity;
-    caseQuantityText.value = caseQuantity;
-  }
-}
-
 document.getElementById("case-plus").addEventListener("click", function () {
-  updateCaseNumber(true);
+  updatePhonePrice(true, casePriceText, casePrice, caseQuantityText, caseQuantity++);
 });
 
 document.getElementById("case-minus").addEventListener("click", function () {
-  updateCaseNumber(false);
+  updatePhonePrice(false, casePriceText, casePrice, caseQuantityText, caseQuantity--);
 });
+
+//phone and case function
+function updatePhonePrice(increase, priceText, price, quantityText, quantity) {
+  if (increase == true) {
+    priceText.innerText = price * ++quantity;
+    quantityText.value = quantity;
+  } else if (quantity > 0) {
+    priceText.innerText = price * --quantity;
+    quantityText.value = quantity;
+  }
+  totalPricePhone();
+}
+
+//Updating total Price
+//total price
+const totalPriceText = document.getElementById("total");
+const totalPrice = parseFloat(totalPriceText.innerText);
+
+//tax
+const totalTaxText = document.getElementById("tax");
+
+//subtotal
+const subTotalPriceText = document.getElementById("subtotal");
+
+function totalPricePhone() {
+
+  const phonePriceTotal = phonePrice * phoneQuantity;
+  const casePriceTotal = casePrice * caseQuantity;
+  const result = phonePriceTotal + casePriceTotal;
+  totalPriceText.innerText = result;
+
+  //taxtotal
+  const totalTax = (result * 10) / 100;
+  totalTaxText.innerText = totalTax.toFixed(2);
+
+  //subtotal
+
+  const subTotal = result + totalTax;
+  subTotalPriceText.innerText = subTotal.toFixed(2);
+}
